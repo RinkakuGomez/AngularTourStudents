3813a8328a5544c334ac71b71053249caa0991a8
JavierGomez <javier.gomez2@soprasteria.com>
2021-06-10 10:48:18 +0200

    added initial code for Driven Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Component
+import { StudentsComponent } from './students/students.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
 
+const routes: Routes = [
+  {path: '', redirectTo: '/Dashboard', pathMatch: 'full' },
+  {path: 'Dashboard', component: DashboardComponent },
+  {path: 'Students', component: StudentsComponent},
+  {path: 'Detail/:id', component: StudentDetailComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
---
+++ src/app/student.service.ts
@@ -0,0 +1,148 @@
+import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+// Component
+import {Student} from './student';
+
+// Service
+import {STUDENTS} from './mock-students';
+
+//Mock
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private studentUrl = 'api/students';  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient, private messageService: MessageService) {}
+
+  /* 
+  * OLD VERSION METHOD
+  */
+  getOldStudents(): Observable<Student[]>{
+    const students = of(STUDENTS);
+    this.messageService.add('StudentService: fetched students');
+
+    return students;
+  }
+
+  getOldStudent(id:number): Observable<Student>{
+    const student = STUDENTS.find(s => s.id === id)!;
+    this.messageService.add('StudentService: fetched Student id=${id}');
+
+    return of(student);
+  }
+  
+  /*
+  * NEW METHOD
+  * USE HTTP API
+  */
+
+  /* GET students from the server */
+  getStudents(): Observable<Student[]> {
+    return this.http.get<Student[]>(this.studentUrl)
+    .pipe(
+      tap(_ => this.log('fetched students')),
+      catchError(this.handleError<Student[]>('getStudents', []))
+    );
+  }
+
+  /** GET hero by id. Return `undefined` when id not found */
+  getStudentNo404(id: number): Observable<Student> {
+    const url = '${this.studentUrl}/?id=${id}';
+    
+    return this.http.get<Student[]>(url).pipe(
+      map(students => students[0]),
+      tap(s => {
+          const outcome = s ? `fetched` : `did not find`;
+          this.log('${outcome} hero id=${id}');
+        }),
+      catchError(this.handleError<Student>('getStudent id=${id}'))
+    );
+  }
+
+  /* GET hero by id. Will 404 if id not found */
+  getStudent(id: number): Observable<Student> {
+    const url = '${this.studentUrl}/${id}';
+    
+    return this.http.get<Student>(url).pipe(
+      tap(_ => this.log('fetched Student id=${id}')),
+      catchError(this.handleError<Student>('getStudent id=${id}'))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateStudent(student: Student): Observable<any> {
+    return this.http.put(this.studentUrl, student, this.httpOptions).pipe(
+      tap(_ => this.log(`updated student id=${student.id}`)),
+      catchError(this.handleError<any>('updateStudent'))
+    );
+  }
+
+  /** POST: add a new student to the server */
+  addStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(this.studentUrl, student, this.httpOptions).pipe(
+      tap((newstudent: Student) => this.log(`added student w/ id=${newstudent.id}`)),
+      catchError(this.handleError<Student>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteStudent(id: number): Observable<Student> {
+    const url = `${this.studentUrl}/${id}`;
+
+    return this.http.delete<Student>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted Student id=${id}`)),
+      catchError(this.handleError<Student>('deleteStudent'))
+    );
+  }
+
+  /* GET heroes whose name contains search term */
+  searchStudents(term: string): Observable<Student[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Student[]>(`${this.studentUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found students matching "${term}"`) :
+        this.log(`no students matching "${term}"`)),
+      catchError(this.handleError<Student[]>('searchStudents', []))
+    );
+  }
+  /* 
+  *AUXILIAR METHODS
+  */
+  /**
+  * Handle Http operation that failed.
+  * Let the app continue.
+  * @param operation - name of the operation that failed
+  * @param result - optional value to return as the observable result
+  */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  /* Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`StudentService: ${message}`);
+  }
+}
